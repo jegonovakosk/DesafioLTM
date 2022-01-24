5a8ffd46c3d3ddc018a911f8ebc56c9e5c790a5b
JegoANovakosk <82410492+JegoNovakoskLtm@users.noreply.github.com>
2022-01-24 11:29:13 -0300

    Adicionar arquivos de projeto.

---
+++ desafio/EncontrarValor.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace desafio
+{
+    class EncontrarValor
+    {
+        public int[] Valores { get; }
+        public int alvo { get; }
+
+        public EncontrarValor(int[] valores, int alvo)
+        {
+            Valores = valores;
+            this.alvo = alvo;
+        }
+
+        public void valorAlvo(int[] valor , int alvo)
+        {
+            int i = 0;
+
+            do
+            {
+                for (int k = 0; k < valor.Length; k++)
+                {
+                    if (valor[i] != valor[k])
+                    {
+                        int res = valor[i] + valor[k];
+                        if (res == alvo)
+                        {
+                            Console.WriteLine(res);
+                        }
+                    }
+                }
+
+                i++;
+            } while (i < valor.Length);
+        }
+    }
+}
---
+++ desafio/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace desafio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         
+            int[] valor = new int[9] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            int[] valor1 = new int[] { 3, 8, 9, 6, 8, 5 };
+            int[] terceiraArray = new int[] { 4, 8, 6, 1, 3, 7 };
+      
+            
+            var valores = new EncontrarValor(valor, 10);
+            var segundoValor = new EncontrarValor(valor1, 10);
+            var terceiraValor = new EncontrarValor(terceiraArray, 7);
+
+            Console.WriteLine("------------------ Primeiro Array ---------------------------");
+            valores.valorAlvo(valor, 10);
+            Console.WriteLine(" ");
+
+            Console.WriteLine("------------------ Segundo Array ---------------------------");
+            segundoValor.valorAlvo(valor1, 10);
+            Console.WriteLine(" ");
+
+            Console.WriteLine("------------------ Terceiro Array ---------------------------");
+            terceiraValor.valorAlvo(terceiraArray, 7);
+            Console.WriteLine(" ");
+
+        }
+    }
+}
